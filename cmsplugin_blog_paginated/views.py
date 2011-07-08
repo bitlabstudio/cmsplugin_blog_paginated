d171eeac8dbd0d6e9e8468e81a83ea49a97b5ded
Martin Brochhaus <mbrochh@gmail.com>
2011-07-08 11:05:44 +0800

    Only show published news

--- cmsplugin_blog_paginated/views.py
+++ cmsplugin_blog_paginated/views.py
@@ -6,6 +6,7 @@ from cmsplugin_blog.models import Entry
 
 class BlogArchiveIndexView(ArchiveIndexView):
     model = Entry
+    queryset = Entry.objects.filter(is_published=True)
     date_field = 'pub_date'
 
     def get_paginate_by(self, queryset):
